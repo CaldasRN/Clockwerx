fd66af9713b9532b6ccadf2354705c05f75d4228
caldasrn <caldasrn@gmail.com>
2019-07-26 16:51:31 -0400

    Added 'pause' and 'stop' functions for timer.  Created Syncronization method to set time on clock.  Created a toggle for military time.

--- src/clocksapp/urls.py
+++ src/clocksapp/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^powerBtn/$', views.powerBtn),
+    url(r'^powerBtn/', views.powerBtn),
     url(r'^timer/$', views.timer),
+    url(r'^sync/$', views.sync),
+    url(r'^pause/$', views.pause),
+    url(r'^stop/$', views.stop),
+    url(r'^milTime/$', views.milTime),
 ]
--- src/clocksapp/views.py
+++ src/clocksapp/views.py
@@ -1,39 +1,34 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from datetime import datetime
 import os
 import time
 import sys
-"""
-#Test
-def austin(request):
-    return HttpResponse('Hi')
-"""
+
+#Assign string for simplified command construction
+send = "irsend SEND_ONCE lircd.conf KEY_"
+
+#Compose Control command
+cd_set = send + "CD-SET"
+mode = send + "MODE"
+play = send + "PLAY"
+ret = send + "RETURN"
+t_set = send + "T-SET"
+power = send + "POWER"
+
 #Press power button on clock
 def powerBtn(request):
     os.system('sudo kill $(pidof lircd)')
     time.sleep(1)
     os.system('sudo lircd --device /dev/lirc0')
     time.sleep(1)
-    os.system('irsend SEND_ONCE lircd.conf KEY_POWER')
-    return HttpResponse('')
+    os.system(power)
+    return HttpResponse('Power')
 
 #Set timer on clock
 def timer(request):
-    #Assign string for simplified command construction
-    send = "irsend SEND_ONCE lircd.conf KEY_"
 
-    #Convert provided ints to string to access each digit
-    time = str(request.GET.get('time'))
-
-#    hour = str(request.GET.get('hour'))
-#    min = str(request.GET.get('min'))
-#    sec = str(request.GET.get('sec'))
-
-#    hour = str(hour)
-#    min = str(min)
-#    sec = str(sec)
+    #Get Time String
+    time = request.GET.get('time')
 
     #Compose Time digits
     digit1 = send + time[0]
@@ -43,12 +38,7 @@ def timer(request):
     digit5 = send + time[6]
     digit6 = send + time[7]
 
-    #Compose Control command
-    cd_set = send + "CD-SET"
-    mode = send + "MODE"
-    play = send + "PLAY"
-
-    #Set timer with provided perameters
+    #Set timer with provided parameters
     os.system(cd_set)
     os.system(cd_set)
     os.system(mode)
@@ -62,4 +52,61 @@ def timer(request):
     os.system(digit5)
     os.system(digit6)
     os.system(play)
-    return HttpResponse('')
+    return HttpResponse('Timer')
+
+#Syncronize clock
+def sync(request):
+
+    #Get current time from RPi
+    currentTime = str(datetime.now())
+    month = int(currentTime[5]) * 10 + int(currentTime[6])
+
+    if month < 3 or month > 10:
+        #Convert form UTC time to EST time
+        Easttime = str((int(currentTime[11]) * 10 + int(currentTime[12]) + 19) % 24)
+    else:
+        #Convert form UTC time to EDT time
+        Easttime = str((int(currentTime[11]) * 10 + int(currentTime[12]) + 20) % 24)
+
+    #Compose Time digits
+    digit1 = send + Easttime[0]
+    digit2 = send + Easttime[1]
+    digit3 = send + currentTime[14]
+    digit4 = send + currentTime[15]
+    digit5 = send + currentTime[17]
+    digit6 = send + currentTime[18]
+
+    #Set clock to current time
+    os.system(t_set)
+    os.system(digit1)
+    os.system(digit2)
+    os.system(mode)
+    os.system(digit3)
+    os.system(digit4)
+    os.system(mode)
+    os.system(digit5)
+    os.system(digit6)
+    os.system(ret)
+    return HttpResponse('Time')
+
+#Pause Timer
+def pause(request):
+
+    os.system(play)
+    return HttpResponse('Pause')
+
+#Stop Timer and show time
+def stop(request):
+
+    os.system(cd_set)
+    os.system(cd_set)
+    os.system(cd_set)
+    os.system(ret)
+    return HttpResponse('Stop')
+
+#Toggle between Military time and Regular time
+def milTime(request):
+
+    mil = send + '0'
+    os.system(mil)
+    return HttpResponse('MilTime')
